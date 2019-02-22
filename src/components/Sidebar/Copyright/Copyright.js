9f801f9886685fb23b6d2de55a149be14eeb45b4
Robert Ying <yingrui205@gmail.com>
2019-02-22 22:24:26 +0800

    Update CC license img style

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './Copyright.module.scss';
 
 const Copyright = ({ copyright }) => (
-  <div className={styles['copyright']}>
-    <div>{copyright}</div>
+  <div
+    className={styles['copyright']}
+    style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+  >
+    <div style={{ marginRight: 10 }}>{copyright}</div>
     <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
       <img
         alt="Creative Commons License"
