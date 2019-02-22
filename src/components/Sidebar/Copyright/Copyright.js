f4cab1d0ec1b5535b4078aa048f435860d050875
Robert Ying <yingrui205@gmail.com>
2019-02-22 21:23:56 +0800

    Use CC license

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -3,7 +3,13 @@ import styles from './Copyright.module.scss';
 
 const Copyright = ({ copyright }) => (
   <div className={styles['copyright']}>
-    {copyright}
+    <div>{copyright}</div>
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+      <img
+        alt="Creative Commons License"
+        src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png"
+      />
+    </a>
   </div>
 );
 
