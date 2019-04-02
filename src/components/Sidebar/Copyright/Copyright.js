e1c65c4b84c856c94c817cf9ad29977077becca5
Robert Ying <yingrui205@gmail.com>
2019-04-03 05:00:04 +0800

    Use text for CC license

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -7,11 +7,8 @@ const Copyright = ({ copyright }) => (
     style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
   >
     <div style={{ marginRight: 10 }}>{copyright}</div>
-    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-      <img
-        alt="Creative Commons License"
-        src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png"
-      />
+    <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">
+      CC BY-NC-SA 4.0
     </a>
   </div>
 );
