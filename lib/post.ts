f5c2f803e79da63b08e660aa0d8c024164c215bb
Rui Ying <yingrui205@gmail.com>
2021-02-04 21:55:25 +0800

    Use frontmatter for metadata

--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
 
 const postsDirectory = path.join(process.cwd(), "pages/posts");
 
@@ -16,32 +17,13 @@ export function getSortedPostsData() {
   const allPostsData = fileNames.map((fileName) => {
     const id = fileName.replace(/\.mdx$/, "");
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const fileContent = fs.readFileSync(fullPath, "utf-8");
 
-    const ast = require("@babel/parser").parse(
-      fileContents
-        .split("\n\n")
-        .find((t) => t.startsWith("export const metadata")),
-      {
-        sourceType: "module",
-      }
-    );
-    const metadataAst = ast.program.body[0].declaration.declarations.find(
-      (d: any) => d.id.name === "metadata"
-    );
-    const properties = metadataAst.init.properties;
-
-    const metadata = properties.reduce(
-      (acc: any, cur: any) => ({
-        ...acc,
-        [cur.key.name]: cur.value.value,
-      }),
-      {}
-    );
+    const { data } = matter(fileContent);
 
     return {
       id,
-      ...metadata,
+      ...data,
     } as PostData;
   });
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withPlugins = require("next-compose-plugins");
+
 const withMDX = require("@next/mdx")({
   options: {
     remarkPlugins: [require("remark-unwrap-images")],
@@ -6,6 +8,35 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "mdx"],
-});
+const withFrontmatter = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      config.module.rules = config.module.rules.map((rule) => {
+        if (rule.test && rule.test.test(".mdx")) {
+          return {
+            ...rule,
+            use: [...rule.use, require.resolve("./plugins/frontmatter")],
+          };
+        } else {
+          return rule;
+        }
+      });
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+
+      return config;
+    },
+  });
+};
+
+module.exports = withPlugins([
+  [withFrontmatter],
+  [
+    withMDX,
+    {
+      pageExtensions: ["ts", "tsx", "mdx"],
+    },
+  ],
+]);
