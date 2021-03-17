208b154d9c6c1e075622d8f0bb8babc65b8d8239
Rui Ying <yingrui205@gmail.com>
2021-03-17 22:01:40 +0800

    Make profile image high priority

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,11 +4,12 @@ import { name, siteDescription } from "lib/meta";
 
 const ProfileImage = ({ size }: { size?: "small" | "large" }) => (
   <Image
-    src="/images/profile.jpg"
     className="rounded-full"
+    src="/images/profile.jpg"
     alt={name}
     width={size === "small" ? 96 : 192}
     height={size === "small" ? 96 : 192}
+    priority
   />
 );
 
