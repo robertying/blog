bf681eff22f62e69fe3447a12aa8e7983e108e05
Rui Ying <yingrui205@gmail.com>
2021-07-25 19:47:50 +0800

    Update site description

--- lib/meta.ts
+++ lib/meta.ts
@@ -1,4 +1,4 @@
 export const name = "Rui Ying (应睿)";
 export const siteName = "Blog by Rui Ying (应睿)";
 export const siteDescription =
-  "EE undergraduate at Tsinghua University. Incoming CS MS graduate at Stanford University. I love computer stuff.";
+  "EE undergrad at Tsinghua; CS masters at Stanford. I love computer stuff.";
