70ca595860156ae3e0bba3bec57c184aa0b8a919
Rui Ying <git@ruiying.io>
2022-06-12 23:44:06 -0700

    Add url shortener

---
+++ pages/_middleware.ts
@@ -0,0 +1,17 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+const redirects: any = {
+  "/resume": "https://dl.ruiying.io/Resume_Rui_Ying_EN.pdf",
+};
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const url = req.nextUrl.pathname;
+
+  if (redirects[url]) {
+    const response = NextResponse.redirect(redirects[url], 301);
+    response.headers.set("cache-control", "private, max-age=90");
+    return response;
+  }
+
+  return NextResponse.next();
+}
