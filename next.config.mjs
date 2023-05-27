3d69b39cfd6156df6c64dbb5e096ca45ec93d23b
Rui Ying <git@ruiying.io>
2023-05-27 04:09:05 -0700

    Opt out of avif

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 31536000,
-  },
   output: "standalone",
   compress: true, // FIXME
 };
