4928eb2f9894b69981018bbac42e9b66b5e7081e
Rui Ying <git@ruiying.io>
2023-05-31 20:47:15 -0700

    Import profile image locally

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { name } from "lib/meta";
+import profileImage from "public/images/profile.jpg";
 
 const ProfileImage = ({ size }: { size?: "small" | "large" }) => (
   <Image
     className="rounded-full"
-    src="/images/profile.jpg"
+    src={profileImage}
     alt={name}
     width={size === "small" ? 96 : 192}
     height={size === "small" ? 96 : 192}
