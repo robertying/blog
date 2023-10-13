7a2b3637ca44f5367d0b364153349b6711e525b3
Rui Ying <git@ruiying.io>
2023-10-13 10:26:06 +0000

    Fix highlight and react rendering in markdown

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -1,3 +1,4 @@
+import * as prod from "react/jsx-runtime";
 import { unified } from "unified";
 import markdown from "remark-parse";
 import breaks from "remark-breaks";
@@ -5,12 +6,15 @@ import remark2rehype from "remark-rehype";
 import slug from "rehype-slug";
 import imgSize from "rehype-img-size";
 import highlight from "rehype-highlight";
+import { common } from "lowlight";
 import groovy from "highlight.js/lib/languages/groovy";
-import { Fragment, createElement } from "react";
 import react from "rehype-react";
 import Image from "next/image";
 import Link from "next/link";
 import isAbsoluteUrl from "is-absolute-url";
+
+// @ts-expect-error: the react types are missing.
+const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs };
 
 export const getReactElementFromMarkdown = async (md: string) => {
   const file = await unified()
@@ -19,10 +23,9 @@ export const getReactElementFromMarkdown = async (md: string) => {
     .use(remark2rehype, { allowDangerousHtml: false })
     .use(slug)
     .use(imgSize, { dir: "public" })
-    .use(highlight, { languages: { groovy } })
+    .use(highlight, { detect: true, languages: { ...common, groovy } })
     .use(react, {
-      createElement: createElement,
-      Fragment: Fragment,
+      ...production,
       components: {
         img: (props: any) => <Image {...props} alt={props.alt!} />,
         a: (props: any) =>
