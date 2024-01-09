97aa071905cd2b53a723240113aae63f53f119db
Rui Ying <git@ruiying.io>
2024-01-09 01:29:03 -0800

    Update site description

--- lib/meta.ts
+++ lib/meta.ts
@@ -1,4 +1,3 @@
 export const name = "Rui Ying (应睿)";
 export const siteName = "Blog by Rui Ying (应睿)";
-export const siteDescription =
-  "CS master’s student at Stanford. I love computer stuff.";
+export const siteDescription = "A Software Engineer 💻 🎮 🍿 🥘";
